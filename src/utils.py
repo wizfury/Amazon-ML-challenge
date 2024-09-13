50c4db4386d732463ff201d348b4f247c4e0a0c8
wizfury <21052721@kiit.ac.in>
2024-09-14 02:53:42 +0530

    update src

--- src/utils.py
+++ src/utils.py
@@ -73,7 +73,7 @@ def download_images(image_links, download_folder, allow_multiprocessing=True):
         download_image_partial = partial(
             download_image, save_folder=download_folder, retries=3, delay=3)
 
-        with multiprocessing.Pool(64) as pool:
+        with multiprocessing.Pool(61) as pool:
             list(tqdm(pool.imap(download_image_partial, image_links), total=len(image_links)))
             pool.close()
             pool.join()
